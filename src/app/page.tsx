2961d561f05642121610606ee79a4097e6e40d1c
thaneswara <thaneswara31@gmail.com>
2025-03-16 19:29:41 +0530

    fixing bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
-import Categories from './components/Hero'
+import Categories from './components/Categories'
 
 const page = () => {
   return (
